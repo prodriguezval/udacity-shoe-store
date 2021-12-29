65dea3fa9deba3509b4b77bbff1e32129874abb9
Pablo Rodriguez <pablo.rodriguez@thoughtworks.com>
2021-12-29 09:40:22 -0300

    Change login screen to constraint layout

--- app/src/main/java/com/udacity/shoestore/shoe/screens/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/shoe/screens/ShoeListFragment.kt
@@ -1,13 +1,13 @@
 package com.udacity.shoestore.shoe.screens
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
 import com.udacity.shoestore.databinding.FragmentShoeListDetailBinding
@@ -27,8 +27,17 @@ class ShoeListFragment : Fragment() {
 
         viewModel.getShoes().observe(viewLifecycleOwner, { shoes ->
             shoes.forEach { shoe ->
-                val shoeItemBinding = DataBindingUtil.inflate<FragmentShoeListDetailBinding>(inflater, R.layout.fragment_shoe_list_detail, container, false)
-                val imageName = resources.getIdentifier("com.udacity.shoestore:drawable/${shoe.images[0]}", null, null)
+                val shoeItemBinding = DataBindingUtil.inflate<FragmentShoeListDetailBinding>(
+                    inflater,
+                    R.layout.fragment_shoe_list_detail,
+                    container,
+                    false
+                )
+                val imageName = resources.getIdentifier(
+                    "com.udacity.shoestore:drawable/${shoe.images[0]}",
+                    null,
+                    null
+                )
                 shoeItemBinding.imgShoe.setImageResource(imageName)
                 shoeItemBinding.txtShoeName.text = (shoe.name)
                 shoeItemBinding.txtShoeDescription.text = (shoe.description)
@@ -36,10 +45,22 @@ class ShoeListFragment : Fragment() {
             }
         })
 
+        setHasOptionsMenu(true)
+
         binding.btnAddShoe.setOnClickListener {
             findNavController().navigate(R.id.action_shoeListFragment_to_shoeDetailFragment)
         }
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
